397ec0e03841e8d3b4d96b7b7878b5ba0be6deb0
jeff <jdagnogo11@gmail.com>
2023-02-17 10:22:06 +0200

    app created

---
+++ Tasty/Tasty/TastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyApp.swift
+//  Tasty
+//
+//  Created by Jean francois dagnogo on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
